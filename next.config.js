9003eb3d7f829aaee1312846e53236986d6d9647
ik04 <ishaankhurana123@gmail.com>
2024-01-08 05:48:10 +0530

    fix: fixed export error

--- next.config.js
+++ next.config.js
@@ -1,66 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // target: "experimental-serverless-trace", // Add this line
-//   output: "export",
-//   // experimental: {
-//   // appDir: true,
-//   // },
-// };
-// module.exports = nextConfig;
-
-// chatGPT;
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {},
-// };
-
-// module.exports = nextConfig;
-
-// docs
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: "export",
-
-//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//   // trailingSlash: true,
-
-//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//   // skipTrailingSlashRedirect: true,
-
-//   // Optional: Change the output directory `out` -> `dist`
-//   // distDir: 'dist',
-// };
-
-// module.exports = nextConfig;
-
-// ***************************************************************chatgpt
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: "export",
-// };
-
-// module.exports = nextConfig;
-// next.config.js
-// next.config.js
